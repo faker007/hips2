2b9e3f8f7f196fe9336d75631a89c6ba41de0469
hanboram.jang <janghan3150@gmail.com>
2017-05-29 18:13:44 +0900

    트랜딩 태그에서 태그 추가할 때, 중복체크.

--- src/app/hips/main/search-input/search-input.component.ts
+++ src/app/hips/main/search-input/search-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'hips-search-input',
@@ -9,13 +10,13 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 export class SearchInputComponent implements OnInit {
   @Input() ref;
   atarashi_array: Array<any> = []; // 태그 검색이 반환될 배열
-  undo_array: Array<any> = []; // 태그 검색을 하면 원본 배열이 사라지는데, 사라지는 원본 배열에 대한 백업용  
+  undo_array: Array<any> = []; // 태그 검색을 하면 원본 배열이 사라지는데, 사라지는 원본 배열에 대한 백업용
 
 	search_queries = ['띄어쓰기', '기준으로', '태그화', '됩니다.'];
 
   eventListIndex:number = 0;; // eventListIndex 변수는 Array.prototype.filter에서 index를 가져올 수 없어서 이렇게 선언 해두었음. 나중에 리펙토링할 수 있으면, 하는 게 좋을듯.
 
-  constructor() { 
+  constructor() {
 
   }
 
@@ -24,6 +25,12 @@ export class SearchInputComponent implements OnInit {
   }
 
   addSearchQueries(word) {
+
+    //트랜딩 태그에서 태그 추가할 때, 중복체크.
+    for(let query of this.search_queries){
+      if(query === word)
+        return;
+    }
   	this.search_queries.push(word);
   }
 
@@ -54,7 +61,7 @@ export class SearchInputComponent implements OnInit {
               console.log(query);
               console.log(tag);
               console.log(eventList.title);
-              console.log(this.eventListIndex);          
+              console.log(this.eventListIndex);
             }
           }
         });
