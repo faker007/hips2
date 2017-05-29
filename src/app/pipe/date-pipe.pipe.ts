4112b5f4d36c22b4ea6ea1c652aba7086c3398f2
LeeGyeJin <rlaqjadud195@naver.com>
2017-05-30 00:29:03 +0900

    줄 맞춤

--- src/app/pipe/date-pipe.pipe.ts
+++ src/app/pipe/date-pipe.pipe.ts
@@ -6,28 +6,29 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DatePipePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    let spliter = value.split(' ');
-    let dates = spliter[0].split('-');
-    let times = spliter[1].split(':');
+  	if(value !== undefined) {
+	    let spliter = value.split(' ');
+	    let dates = spliter[0].split('-');
+	    let times = spliter[1].split(':');
 
-    let year = dates[0] + "년";
-    let month = dates[1] + "월";
-    let day = dates[2] + "일";
+	    let year = dates[0] + "년";
+	    let month = dates[1] + "월";
+	    let day = dates[2] + "일";
 
-    let hour = times[0];
-    let minutes = times[1];
+	    let hour = times[0];
+	    let minutes = times[1];
 
-    let timeSpliter = ''; // 오전 또는 오후
+	    let timeSpliter = ''; // 오전 또는 오후
 
-    if(hour / 12 <= 1) {
-    	timeSpliter = "오전"
-    } else {
-    	timeSpliter = "오후"
-    	hour = hour - 12;
-    }
+	    if(hour / 12 <= 1) {
+	    	timeSpliter = "오전"
+	    } else {
+	    	timeSpliter = "오후"
+	    	hour = hour - 12;
+	    }
 
-    let returned = `${year} ${month} ${day} ${timeSpliter}${hour}:${minutes}`;
-    return returned;
+	    let returned = `${year} ${month} ${day} ${timeSpliter}${hour}:${minutes}`;
+	    return returned;  		
+  	}
   }
-
 }
