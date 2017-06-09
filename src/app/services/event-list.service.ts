e70b0921aa6953b73518437eb394e0f32cc336d3
GyeJin Lee(이 계진) <shine__smart@naver.com>
2017-06-09 19:14:43 +0900

    오늘을 기준으로 행사 가져오는 함수 추가.

--- src/app/services/event-list.service.ts
+++ src/app/services/event-list.service.ts
@@ -13,11 +13,21 @@ export class EventListService {
   my_events: Subject<Array<any>>;
   eventCount: number = 0;
 
+  date: any;
+  todayYear: any;
+  todayMonth: any;
+  todayDay: any;
+
   constructor(public db: AngularFireDatabase) {
     this.events = this.db.list('/event', { preserveSnapshot: true });
     this.name = new Subject();
   	this.status = new Subject();
     this.my_events = new Subject();
+
+    this.date = new Date();
+    this.todayYear = this.date.getFullYear();
+    this.todayMonth = this.date.getMonth() + 1;
+    this.todayDay = this.date.getDate();    
   }
 
   getEvents(): any {
@@ -32,7 +42,7 @@ export class EventListService {
     this.my_events.next(events);
   }
 
-  getEventsByDate(begin, end): any{
+  getEventsByDate(begin, end): any {
     return this.db.list('/event', { 
       query: {
         orderByChild: 'begin',
@@ -42,14 +52,32 @@ export class EventListService {
     });
   }
 
-  getEventsNumber(count: number) {
+  addZero(argu) {
+    if(parseInt(argu) < 10) {
+      return "0" + argu.toString().trim()
+    } else {
+      return argu;
+    }
+  }  
+
+  getEventsNumber(count: number): any {
     this.eventCount = this.eventCount + count;
     return this.db.list('/event', { 
       query: {
         orderByChild: 'begin',
-        startAt: '2017-06-07',
+        startAt: `${this.todayYear}-${this.addZero(this.todayMonth)}-${this.addZero(this.todayDay)}`,
         limitToFirst: this.eventCount
       }, preserveSnapshot: true 
+    });
+  }
+
+  getTodayEvents(): any {
+    return this.db.list('event', {
+      query: {
+        orderByChild: 'begin',
+        startAt: `${this.todayYear}-${this.todayMonth}-${this.todayDay}`,
+        limitToFirst: 1000
+      }, preserveSnapshot: true
     });
   }
 }
